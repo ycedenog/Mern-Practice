daa9d56712fd0a668d1a1ef3255ab5b7b2cd8b82
ycedenog <yonmaced@espol.edu.ec>
2022-03-26 01:29:39 -0500

    Funciones borrar, actualizar y añadir

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -10,8 +10,51 @@ router.get('/', async (req, res) => {
     res.json(tasks);
 });
 
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+
+})
+
 //Metodo post que nos permite agregar datos a traves de Http
 router.post('/', async (req, res) => {
-    console.log(req.body);
+    const {
+        title,
+        description
+    } = req.body;
+    const task = new Task({
+        title,
+        description
+    });
+    await task.save();
+    res.json({
+        status: 'Task Saved'
+    });
 })
+
+//Metodo put para actualizar datos a traves de Http
+
+router.put('/:id', async (req, res) => {
+    const {
+        title,
+        description
+    } = req.body;
+    const newTask = {
+        title,
+        description
+    };
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({
+        status: 'Task Updated'
+    });
+});
+
+//Metodo delete para borrar datos 
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({
+        status: 'Task Deleted'
+    });
+});
+
 module.exports = router;
